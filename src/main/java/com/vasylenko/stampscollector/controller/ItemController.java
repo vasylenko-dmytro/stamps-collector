6b2104f005e6ae835476e12bc79914af1c65ca6e
vasylenko <dmitriy.vasylenko@nixsolutions.com>
2023-07-17 00:55:21 +0300

    Add get Item by id method

--- src/main/java/com/vasylenko/stampscollector/controller/ItemController.java
+++ src/main/java/com/vasylenko/stampscollector/controller/ItemController.java
@@ -1,11 +1,13 @@
 package com.vasylenko.stampscollector.controller;
 
 import com.vasylenko.stampscollector.domain.Item;
+import com.vasylenko.stampscollector.exception.ResourceNotFoundException;
 import com.vasylenko.stampscollector.service.ItemService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -28,5 +30,12 @@ public class ItemController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+
+    @GetMapping("/items/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable("id") Long id) {
+        Item item = itemService.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Item with id = " + id));
+        return new ResponseEntity<>(item, HttpStatus.OK);
     }
 }
