b60421a17397d877d1a4225d2e8eefd372779b41
vasylenko <dmitriy.vasylenko@nixsolutions.com>
2023-07-17 00:54:03 +0300

    Add ResourceNotFoundException exception

---
+++ src/main/java/com/vasylenko/stampscollector/exception/ResourceNotFoundException.java
@@ -0,0 +1,25 @@
+package com.vasylenko.stampscollector.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 3455098976140452018L;
+
+    public ResourceNotFoundException() {
+        super();
+    }
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
