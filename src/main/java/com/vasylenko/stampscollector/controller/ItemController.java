e17ed24ea61f9790db39492452f27f60acffca3c
vasylenko <dmitriy.vasylenko@nixsolutions.com>
2023-07-16 23:59:12 +0300

    Add controller for Item class

---
+++ src/main/java/com/vasylenko/stampscollector/controller/ItemController.java
@@ -0,0 +1,32 @@
+package com.vasylenko.stampscollector.controller;
+
+import com.vasylenko.stampscollector.domain.Item;
+import com.vasylenko.stampscollector.service.ItemService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/api")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping("/items")
+    public ResponseEntity<List<Item>> getItems() {
+        List<Item> items = new ArrayList<>(itemService.findAll());
+        if (items.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+}
